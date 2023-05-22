b2042d7364dc668234bd56e411a79e6280268286
Mrquez <72227934+Mrquez@users.noreply.github.com>
2023-05-22 09:05:38 -0600

    Modificacion frecuencia
    :)

---
+++ Proyecto/js/boton_dias_personalizados.js
@@ -0,0 +1,16 @@
+function showCheckbox() {
+  var checkboxContainer = document.getElementById("checkboxContainer");
+  checkboxContainer.style.display = "block";
+}
+function ocultarCheckbox() {
+  var checkboxContainer = document.getElementById("checkboxContainer");
+  checkboxContainer.style.display = "none";
+}
+function showCheckbox01() {
+  var checkboxContainer01 = document.getElementById("checkboxContainer01");
+  checkboxContainer01.style.display = "block";
+}
+function ocultarCheckbox01() {
+  var checkboxContainer01 = document.getElementById("checkboxContainer01");
+  checkboxContainer01.style.display = "none";
+}
