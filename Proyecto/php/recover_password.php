c0b692c4b232919bf17a042d4d45c03abb3d3a30
Elitrea <65686493+Elitrea@users.noreply.github.com>
2023-06-08 09:50:16 -0600

    act_login.html

--- Proyecto/php/recover_password.php
+++ Proyecto/php/recover_password.php
@@ -1,10 +1,11 @@
 <?php
 include('connect.php');
+
 // Obtener el correo electrónico del formulario de recuperación
 $correo = $_POST['correo'];
 
 // Consultar la tabla de usuarios para verificar el correo electrónico
-$query = "SELECT id FROM usuarios WHERE correo = '$correo'";
+$query = "SELECT idUsuario FROM usuario WHERE correo = '$correo'";
 $result = mysqli_query($conex, $query);
 $row = mysqli_fetch_assoc($result);
 
@@ -13,21 +14,20 @@ if ($row) {
   $codigo = bin2hex(random_bytes(16));
 
   // Guardar el código de recuperación en la base de datos
-  $query = "UPDATE usuarios SET codigo_recuperacion = '$codigo' WHERE correo = '$correo'";
+  $query = "UPDATE usuario SET codigo_recuperacion = '$codigo' WHERE correo = '$correo'";
   mysqli_query($conex, $query);
 
-  // Enviar un correo electrónico al usuario con el código de recuperación
-  // y un enlace a una página donde pueda ingresar un nueva contraseña
+  // Enviar un correo electrónico al usuario con el código de recuperación y un enlace a una página donde pueda ingresar una nueva contraseña
+  $resetLink = "http://ejemplo.com/restablecer.php?codigo=" . $codigo;
   $mensaje = "Hemos recibido una solicitud para restablecer la contraseña de su cuenta.\n\n";
-  $mensaje .= "Para continuar, ingrese el siguiente código de recuperación:\n\n";
-  $mensaje .= "$codigo\n\n";
-  $mensaje .= "Ingrese su nueva contraseña aquí: http://ejemplo.com/restablecer.php\n\n";
+  $mensaje .= "Para continuar, haga clic en el siguiente enlace:\n\n";
+  $mensaje .= "$resetLink\n\n";
   $mensaje .= "Si no solicitó el restablecimiento de su contraseña, ignore este mensaje.\n";
 
-    ini_set("SMTP", "smtp.gmail.com");
-    ini_set("smtp_port", "587");
+  ini_set("SMTP", "smtp.gmail.com");
+  ini_set("smtp_port", "587");
 
-     mail($correo, "Recuperación de contraseña", $mensaje);
+  mail($correo, "Recuperación de contraseña", $mensaje);
 
   echo "Se ha enviado un correo electrónico con las instrucciones para restablecer su contraseña.";
 } else {
