76a2e14c32fa6ae548b96e058d0500fbf4059094
noraarellano <72581638+noraarellano@users.noreply.github.com>
2023-06-15 22:38:31 -0600

    Tareas APARECEN en calendario

--- Proyecto/php/calendario.php
+++ Proyecto/php/calendario.php
@@ -1,12 +1,9 @@
 <?php
 include 'connect.php';
 
-// Realiza la consulta para obtener los eventos desde la base de datos
+// Realiza la consulta para obtener los eventos desde la base de datos (TAREAS)
 $query = "SELECT idtareas, nom_tarea, fecha FROM tareas";
 $stmt = $conex->query($query);
-
-$query2 = "SELECT idhabitos, nom_habito, lunes, martes, miercoles, jueves, viernes, sabado, domingo FROM habitos";
-$stmt2 = $conex->query($query2);
 
 // Prepara un array para almacenar los eventos
 $eventos = [];
@@ -16,20 +13,6 @@ while ($row = $stmt->fetch_assoc()) {
   $evento = [
     'title' => $row['nom_tarea'],
     'start' => $row['fecha']
-  ];
-  $eventos[] = $evento;
-}
-
-while ($row = $stmt2->fetch_assoc()) {
-  $evento = [
-    'title' => $row['nom_habito'],
-    'start' => $row['lunes'],
-    'start' => $row['martes'],
-    'start' => $row['miercoles'],
-    'start' => $row['jueves'],
-    'start' => $row['viernes'],
-    'start' => $row['sabado'],
-    'start' => $row['domingo']
   ];
   $eventos[] = $evento;
 }
