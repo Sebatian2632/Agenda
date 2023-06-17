44baab2440dabe29b3443adcd15feab7ce567f50
Mrquez <cmarquezmoreno@gmail.com>
2023-06-17 00:16:05 -0600

    Tabla calendario
    Se agregó la tabla, carga los hábitos del usuario, read funcionando en la misma tabla, ya no se puede ingresar al calendario si no se tiene sesión iniciada.
    Algo bien :)

--- Proyecto/js/crud_habitos.js
+++ Proyecto/js/crud_habitos.js
@@ -127,6 +127,40 @@ async function actionRead() {
 }
 
 
+  // -----------------  READ TAREAS  ------------------
+// Pone en la tabla todos los registros de la BD pero en el calendario
+async function actionReadCalendario() {
+  const email = await obtenerCorreo();
+
+
+  $.ajax({
+    method:"POST",
+    url: "../php/crud_habitos.php",
+    data: {
+      accion: "read",
+      correo: email,
+
+    },
+    success: function( respuesta ) {
+      JSONRespuesta = JSON.parse(respuesta);
+      console.log(JSONRespuesta.estado);
+
+        tabla = $("#example2").DataTable();
+            JSONRespuesta.entregas.forEach(habitos => {
+              
+              let Botones="";
+                Botones = '<i class="fas fa-eye" style="font-size:25px;color: #af66eb; margin-right: 10px;" data-toggle="modal" data-target="#modal_read_tarea" onclick="actionReadById('+habitos.idhabitos+')"></i>';
+                Botones += '<i class="fas fa-bars" style="font-size:25px;color: #af66eb; margin-right: 10px;" data-toggle="modal" data-target="#modal_estadisticas" onclick=""></i>';
+                Botones += '<br>Ya realicé el hábito hoy &nbsp; <input type="checkbox" id="RealizadoHoy" value="">'
+              tabla.row.add([habitos.nom_habito, Botones]).draw().node().id="renglon_"+habitos.idhabitos;
+            });
+      console.log(respuesta);
+    }
+  });
+}
+
+
+
 // -----------------  READ_BY_ID TAREAS  ------------------
 // Funciona al oprimir el botón de morado de leer para cada tarea, o cuando se selecciona desde el calendario
 function actionReadById(id){
